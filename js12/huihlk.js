8bba0e1affafc4714ad88c96fadbfdcc957bcdfc
PCK49 <160991878+PCK49@users.noreply.github.com>
2025-06-22 08:33:23 -0700

    Update huihlk.js

--- js12/huihlk.js
+++ js12/huihlk.js
@@ -1,3 +1,4 @@
+document.getElementById("demo").innerHTML="something i guss";
 function ffutre() {
   document.getElementById("demo").innerHTML="ihinohgbyidfft7uyodoc8yudxo57tyudef7yiurf79uptguojfguoj";
 }
